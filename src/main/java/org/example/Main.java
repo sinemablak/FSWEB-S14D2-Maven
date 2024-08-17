aca4a0fc692e4b28d13113c41badb81f7035ae94
sinemablak <sinemablakk1@gmail.com>
2024-08-17 23:42:24 +0300

    project finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom=new Bedroom("Yatak odasi",new Wall("kuzey"),new Wall("güney"),
+                new Wall("dogu"),new Wall("bati"),new Ceiling(1, PaintColor.GREEN),
+                new Bed("cift",2,2,2,2),new Lamp(LampType.JAVA,true,4),
+                new Wardrobe(2,3,2),new Carpet(2,2,PaintColor.WHITE));
+        System.out.println(bedroom.toString());
     }
 }
